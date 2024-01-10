ddbae41a977fbbc6baac1432cf146720c508d40e
jetjo <bingsea@foxmail.com>
2024-01-10 10:08:28 +0800

    test array includes

---
+++ src/响应式数据/array/test/1/includes.1.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+测试确认: vue并没有对自定义方法的参数做任何处理
+	</div>
+</template>
+
+<script setup>
+import { reactive, readonly, watchEffect } from "vue";
+
+const raw = [{}, {}]
+const state = reactive(raw)
+
+raw.includes = (...items) => console.log(items);
+
+watchEffect(() => {
+	console.log(state.includes(readonly(state[0]), state[1], state[2]))
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/响应式数据/array/test/1/includes.2.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+测试确认: 即使数组自定义的方法没有对数组做任何操作,即没有依赖数组的任何,只要这个自定义的方法被代理了,仍然像有依赖一样
+	</div>
+</template>
+
+<script setup>
+import { reactive, readonly, watchEffect } from "vue";
+
+const raw = [{}, {}]
+const state = reactive(raw)
+
+raw.includes = (...items) => console.log(items);
+
+watchEffect(() => {
+	console.log(state.includes())
+})
+
+state[1] = 1
+</script>
+
+<style lang="scss" scoped>
+
+</style>
