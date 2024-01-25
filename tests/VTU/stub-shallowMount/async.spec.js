9dde5057697f6d7f78fa6f70b41671733e6bbf92
jetjo <bingsea@foxmail.com>
2024-01-25 11:56:32 +0800

    fix error

--- tests/VTU/stub-shallowMount/async.spec.js
+++ tests/VTU/stub-shallowMount/async.spec.js
@@ -18,24 +18,29 @@ afterEach(() => {
 it('模拟异步组件', async () => {
 	const _wrapper = mount(Async, {
 		// const _wrapper = mount(AsyncComTestHelper, {
+			global: {
+				stubs: {
+					CC: true
+				}
+			}
+		})
+		// await flushPromises();
+		const wrapper = _wrapper//.getComponent(Async);
+		expect(wrapper.html()).toMatchFileSnapshot('./__snapshots__/async.cc.snap.html')
+	})
+	it('模拟异步组件自身', async () => {
+		const wrapper = mount(AsyncComTestHelper, {
+		// const wrapper = mount(Async, {
 		global: {
 			stubs: {
-				CC: true
+				// NOTE: Async组件使用ComplexC组件定义了新的组件CC, 所以这里模拟ComplexC组件不起作用
+				// ComplexC: true,
+				CC: true,
+				// BB: true
 			}
 		}
 	})
+	// NOTE: 如果不使用AsyncComTestHelper组件,借助Suspense组件,是无法等到Async异步渲染完成的
 	// await flushPromises();
-	const wrapper = _wrapper//.getComponent(Async);
-	expect(wrapper.html()).toMatchFileSnapshot('./__snapshots__/async.cc.snap.html')
-})
-it('模拟异步组件自身', async () => {
-	const wrapper = mount(Async, {
-		global: {
-			stubs: {
-				ComplexC: true
-			}
-		}
-	})
-	await flushPromises();
 	expect(wrapper.html()).toMatchFileSnapshot('./__snapshots__/async.ss.snap.html')
 })
