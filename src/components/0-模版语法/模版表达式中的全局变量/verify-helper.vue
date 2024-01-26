15805d52883155fa9a6cf6de1d0f46e50b2d2d4f
jetjo <bingsea@foxmail.com>
2024-01-26 19:34:58 +0800

    模版表达式中的全局变量

---
+++ src/components/0-模版语法/模版表达式中的全局变量/verify-helper.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<p>declaredWithVarInSetup: {{ declaredWithVarInSetup }}</p>
+		<!-- runtime-core.esm-bundler.js:41 [Vue warn]: Property "setOnGlobalThisInSFC" was accessed during render but is not defined on instance.  -->
+		<p>setOnGlobalThisInSFC: {{ setOnGlobalThisInSFC }}</p>
+		<!-- [Vue warn]: Property "notExistOnGlobalThisAtAll" was accessed during render but is not defined on instance.  -->
+		<!-- <p>notExistOnWindowAtAll: {{ notExistOnGlobalThisAtAll }}</p> -->
+		<p>notExistOnWindowAtAll: {{ String(notExistOnGlobalThisAtAll) }}</p>
+		<p>
+			利用`app.config.globalProperties`注册的全局变量: {{ $1CustomGlobalVar }}
+		</p>
+	</div>
+</template>
+
+<script setup>
+	var declaredWithVarInSetup = "var";
+	globalThis.setOnGlobalThisInSFC = ["global"];
+</script>
+
+<style lang="scss" scoped></style>
