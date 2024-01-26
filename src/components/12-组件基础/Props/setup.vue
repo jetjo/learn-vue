650ede7a6b88240ef1ca1d26a9d9cb061f00bafd
jetjo <bingsea@foxmail.com>
2024-01-26 19:34:38 +0800

    v-bind指令简写

---
+++ src/components/12-组件基础/Props/setup.vue
@@ -0,0 +1,23 @@
+<template>
+	<div style="border-bottom: 1px solid #2268c3">
+		<!-- NOTE: 没有从setup方法返回props,是否可用??? -->
+		<!-- {{ props.title }} props未定义 -->
+		<!-- {{ this.title }} -->
+		<p>title: {{ props.title }}</p>
+		<p>content: {{ props.content }}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: ["title", "content"],
+		setup(props) {
+			console.log(props);
+			// NOTE: 是否需要返回props???
+			// 可以不返回,但是如果不返回,父组件传递过来的属性就需要通过this访问了,
+			// 不过这也无所谓,毕竟这就是选项式组件
+			// return {};
+			return { props };
+		},
+	};
+</script>
