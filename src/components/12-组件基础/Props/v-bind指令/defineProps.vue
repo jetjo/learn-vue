4dde28b0ee4539c8a70c8b4b2a86b1f4328dc552
jetjo <bingsea@foxmail.com>
2024-01-26 19:04:59 +0800

    升级vue, 支持v-bind简写

---
+++ src/components/12-组件基础/Props/v-bind指令/defineProps.vue
@@ -0,0 +1,15 @@
+<template>
+	<div style="border-bottom: 1px solid #000">
+		<p>title: {{ state.title }}</p>
+		<p>content: {{ state.content }}</p>
+	</div>
+</template>
+
+<script setup>
+	import { onMounted, reactive } from "vue";
+	const props = defineProps(["title", "content"]); //NOTE: 执行时机: Vue框架预处理阶段
+	const state = reactive({
+		title: props.title,
+		content: props.content,
+	});
+</script>
---
+++ src/components/12-组件基础/Props/v-bind指令/shorthand-verify-helper.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<!-- <SetupSyntaxSugar v-bind:title="title" v-bind:content="content" /> -->
+		<SetupSyntaxSugar :title="title" :content="content" />
+		<!-- Vue3.4版本及以上才支持 -->
+		<SetupSyntaxSugar :title :content />
+		<SetupSyntaxSugar v-bind="allProps" />
+		<OptionalWithSetup :title="title" :content="content" />
+		<!-- Vue3.4版本及以上才支持 -->
+		<OptionalWithSetup :title :content />
+		<OptionalWithSetup v-bind="allProps" />
+	</div>
+</template>
+
+<script setup>
+	import SetupSyntaxSugar from "./defineProps.vue";
+	import OptionalWithSetup from "../setup.vue";
+
+	const title = "摇滚雷鬼谢天笑";
+	const content = "X.T.X";
+
+	const allProps = {
+		title,
+		content,
+	};
+</script>
+
+<style lang="scss" scoped></style>
