d6abf9e11504d4bf5f6e6cf9c70e3260a9173bf9
jetjo <bingsea@foxmail.com>
2024-02-02 18:01:44 +0800

    修复router.js与vue的耦合

--- src/utils/router.js
+++ src/utils/router.js
@@ -1,29 +1,31 @@
-import { createApp, defineAsyncComponent } from "#vue";
+// import { createApp, defineAsyncComponent } from "#vue";
 import { mapValues } from "lodash-es";
-
-function useRouter(map, ele) {
-	let app;
-	map = mapValues(map, (v, k) =>
-		typeof v === "string"
-			? defineAsyncComponent(() => import(v || `#${k}`))
-			: v,
-	);
-	console.log("map:", map);
-	const load = () => {
-		// 获取当前路径的hash
-		const hash = location.hash.slice(1);
-		console.log("hashchange:", hash);
-		// 根据hash获取对应的组件
-		const comp = map[hash] || map[""];
-		app = createApp(comp);
-		app.mount(ele);
-	};
-	// 监听hashchange事件
-	window.addEventListener("hashchange", () => {
-		app.unmount();
+function injectRouter({ createApp, defineAsyncComponent, importUrl }) {
+	return function useRouter(map, ele) {
+		let app;
+		const getPath = (url) =>
+			importUrl ? new URL(url, importUrl).toString() : url;
+		map = mapValues(map, (v, k) =>
+			typeof v === "string"
+				? defineAsyncComponent(() => import(getPath(v) || `#${k}`))
+				: v,
+		);
+		console.log("map:", map);
+		const load = () => {
+			// 获取当前路径的hash
+			const hash = location.hash.slice(1);
+			console.log("hashchange:", hash);
+			// 根据hash获取对应的组件
+			const comp = map[hash] || map[""];
+			app = createApp(comp);
+			app.mount(ele);
+		};
+		// 监听hashchange事件
+		window.addEventListener("hashchange", () => {
+			app.unmount();
+			load();
+		});
 		load();
-	});
-	load();
+	};
 }
-
-export { useRouter };
+export { injectRouter };
